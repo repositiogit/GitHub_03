9bec84b29e6e31ab4b615280e9242a6b8a19947d
repositiogit <126819501+repositiogit@users.noreply.github.com>
2023-03-22 14:46:17 -0500

    cambio titulo
    por Git y GitHub_cabio_05

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Git y GitHub',
+      title: 'Git y GitHub_cabio_05',
       theme: ThemeData(
         primarySwatch: Colors.red,
       ),
