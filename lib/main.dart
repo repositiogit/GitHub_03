98faf5675c72fd275024b69ccf9c69df4aa4d521
Fabio Alvarez Grisales <repositiogit@mail.com>
2023-03-23 00:11:30 -0500

    Cambio titulo por (Cambio_08_09_10)

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Cambio_08_09',
+      title: 'Cambio_08_09_10',
       theme: ThemeData(
         primarySwatch: Colors.red,
       ),
